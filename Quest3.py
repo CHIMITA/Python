ae40fd6305dda4088a095694a752747265a9e406
JiWon <49803467+CHIMITA@users.noreply.github.com>
2021-03-30 12:16:24 +0900

    School to Python

---
+++ Quest3.py
@@ -0,0 +1,21 @@
+
+#도전문제 1 중첩 for문
+
+for i in range(5):
+    print("k", end='')
+    for j in range(5):
+        print("a", end='')
+    print("k")
+
+print("-" *20)
+
+for j in range(6):
+    print("K", end='')
+
+print("")
+
+for k in range(6):
+    print("A", end='')
+
+print("-" *20)
+
