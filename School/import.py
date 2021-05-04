5df9283deb7ad594953531518641ebec75a5a622
JiWon <49803467+CHIMITA@users.noreply.github.com>
2021-05-04 11:56:51 +0900

    import, from, as

---
+++ School/import.py
@@ -0,0 +1,25 @@
+import math as m # as 모듈 이름 지정
+import math
+import sys
+from math import pi
+from math import *
+from math import sqrt as s
+from math import pi as p, sqrt as s
+
+
+print(math.pi) #원주율
+print(math.sqrt(4.0)) #제곱근
+
+print(m.sqrt(4.0))
+
+print(pi)
+print(sqrt(4.0))
+
+print(s(4.0))
+
+print(p) 
+print(s(2.0))
+
+print(sys.builtin_module_names) #파이썬에서 제공하는 표준 모듈 목록 알아보는 것
+
+print(dir(math)) #math 모듈이 제공하는 함수 목록 
