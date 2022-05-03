e19e8f89a900932dbd716119e6eb0d87969c6b14
CHIMITA <smile0375-@naver.com>
2022-05-03 23:52:50 +0900

    def

--- swu/SC/10.py
+++ swu/SC/10.py
@@ -1,11 +1,30 @@
 def plus(a, b):
     sum = a + b
-    return sum
+    print(sum)
+
+def minus(a, b):
+    sum = a - b
+    print(sum)
+
+def multi(a, b):
+    sum = a * b
+    print(sum)
 
 i = int(input("1. 덧셈 , 2. 뺄셈 , 3. 곱셈 , 4. 나눗셈 , 5. 종료 "))
 
-for i in range(i):
-    if i == 1:
-        a = int(input("숫자를 입력하세요 : "))
-        b = int(input("숫자를 입력하세요 : "))
-        plus(a, b)
+if i == 1:
+    a = int(input("숫자를 입력하세요 : "))
+    b = int(input("숫자를 입력하세요 : "))
+    plus(a, b)
+elif i == 2:
+    a = int(input("숫자를 입력하세요 : "))
+    b = int(input("숫자를 입력하세요 : "))
+    minus(a,b)
+elif i == 3:
+    a = int(input("숫자를 입력하세요 : "))
+    b = int(input("숫자를 입력하세요 : "))
+    multi
+elif i == 4:
+    a = int(input("숫자를 입력하세요 : "))
+    b = int(input("숫자를 입력하세요 : "))
+    minus(a,b)
