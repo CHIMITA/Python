84d08478c4e6684a8fb6c780191fe8af3c145185
CHIMITA <smile0375-@naver.com>
2022-05-07 06:44:00 +0900

    tor

--- Tor/crawling.py
+++ Tor/crawling.py
@@ -1,10 +1,11 @@
-import requests
-import re
+import requests #Http 호출 라이브러리 
+import re #정규표현식 모듈
+
 from bs4 import BeautifulSoup
 
 list = []
     
-session = requests.session()
+session = requests.session() # session 활성화
 session.proxies = {}
 session.proxies['http'] = 'socks5h://localhost:9050'
 session.proxies['https'] = 'socks5h://localhost:9050'
@@ -16,15 +17,15 @@ soup = BeautifulSoup(page.content, "lxml")
     
 p = soup.find("div", class_="box-info")
 
+print("[.Onion site list]\n")
+
 for div in p.find_all("div", class_="box-info-detail"):
     for a in p.find_all("a",{"class":"site-heading"}):
         
-        list = soup.find("strong")
+        list = soup.find_all("strong")
     
-        t = re.compile('(?<=\<strong>)(.*?)(?=<\/strong>)')
-        
-        t_list = t.findall(str(list))
-    print("[.Onion site list]\n",t_list)
-
-        
-    
+        t = re.compile('(?<=\<strong>)(.*?)(?=<\/strong>)') # 메타문자 
+       
+        t_list = t.findall(str(list)) 
+      
+    print("[.Onion site list]\n", t_list)
