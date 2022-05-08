11e76f0c938564d1920753b6670237c7c2dd3ff8
CHIMITA <smile0375-@naver.com>
2022-05-08 17:37:39 +0900

    add link

--- Tor/crawling.py
+++ Tor/crawling.py
@@ -1,9 +1,8 @@
+
 import requests #Http 호출 라이브러리 
 import re #정규표현식 모듈
-import socks
 from bs4 import BeautifulSoup
-
-list = []
+import os
     
 #session = requests.session() # session 활성화
 #session.proxies = {}
@@ -17,14 +16,15 @@ proxies ={
     'https' : 'socks5h://127.0.0.1:9150'
 }
 
-res = requests.get(url, proxies=proxies)
-soup = BeautifulSoup(res.content, "lxml")
+res = requests.get(url, proxies=proxies) # url로 get 요청과 프록시 설정값을 보내고 응답 받음
+
+soup = BeautifulSoup(res.content, "lxml") 
     
 p = soup.find("div", class_="box-info")
 
-print("[.Onion site list]\n")
+print("[.Onion site name list]\n")
 
-for div in p.find_all("div", class_="box-info-detail"):
+for div in p.find_all("div", class_="space-10"):
     for a in p.find_all("a",{"class":"site-heading"}):
         
         list = soup.find_all("strong")
@@ -32,5 +32,11 @@ for div in p.find_all("div", class_="box-info-detail"):
         t = re.compile('(?<=\<strong>)(.*?)(?=<\/strong>)') # 메타문자 
        
         t_list = t.findall(str(list)) 
-      
-    print("[.Onion site list]\n", t_list)
+        
+del t_list[0:3] #onion 사이트 명이 아닌 다른 것이 출력되어서 삭제 
+print("\n".join(t_list))
+
+print("[.Onion site Link list]\n")
+
+for href in soup.find("div", class_="box-info").find_all("a",{"class":"site-url"}):
+    print(href["href"])
