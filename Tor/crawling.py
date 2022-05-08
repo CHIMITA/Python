658bf8c550ee1d3911ff8c096389244047b85e68
CHIMITA <smile0375-@naver.com>
2022-05-08 15:07:10 +0900

    END

--- Tor/crawling.py
+++ Tor/crawling.py
@@ -1,19 +1,24 @@
 import requests #Http 호출 라이브러리 
 import re #정규표현식 모듈
-
+import socks
 from bs4 import BeautifulSoup
 
 list = []
     
-session = requests.session() # session 활성화
-session.proxies = {}
-session.proxies['http'] = 'socks5h://localhost:9050'
-session.proxies['https'] = 'socks5h://localhost:9050'
+#session = requests.session() # session 활성화
+#session.proxies = {}
+#session.proxies['http'] = 'socks5h://localhost:9050'
+#session.proxies['https'] = 'socks5h://localhost:9050'
 
-url = 'http://wiki47qqn6tey4id7xeqb6l7uj6jueacxlqtk3adshox3zdohvo35vad.onion/'
-     
-page = session.get(url)
-soup = BeautifulSoup(page.content, "lxml")
+url = "http://wiki47qqn6tey4id7xeqb6l7uj6jueacxlqtk3adshox3zdohvo35vad.onion/"
+
+proxies ={
+    'http' : 'socks5h://127.0.0.1:9150',
+    'https' : 'socks5h://127.0.0.1:9150'
+}
+
+res = requests.get(url, proxies=proxies)
+soup = BeautifulSoup(res.content, "lxml")
     
 p = soup.find("div", class_="box-info")
 
