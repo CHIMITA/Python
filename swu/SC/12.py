e55237c69f144f731a60ab47f7272a7b6db71c61
CHIMITA <smile0375-@naver.com>
2022-05-12 22:44:06 +0900

    module

---
+++ swu/SC/12.py
@@ -0,0 +1,16 @@
+#정보보호학과 2022111321 박지원
+#문제 3번 
+
+#모듈 호출
+import square
+
+#사용자에게 입력 받기
+square_h = int(input("사각형의 가로 길이를 입력하시오. : "))
+square_l = int(input("사각형의 세로 길이를 입력하시오. : "))
+
+#모듈 사용
+square.square_a(square_h, square_l)
+square.square_r(square_h, square_l)
+
+
+
---
+++ swu/SC/square.py
@@ -0,0 +1,11 @@
+#넓이 구하는 함수
+def square_a(square_h, square_l):
+    sum = square_h * square_l
+    print("사각형의 넓이는 ", sum, "입니다.")
+
+    return sum
+
+#둘레 구하는 함수
+def square_r(square_h, square_l):
+    sum = square_h + square_l * 2
+    print("사각형의 둘레는 ", sum, "입니다.")
